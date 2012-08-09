8be0a2969df5c535cb294a6cbabf754c98f204ed
alurcard2 <alurcard2@gmail.com>
2012-08-08 22:46:50 -0700

    LiteStep no longer freezes when a task becomes unresponsive.

--- nTasks/WindowManager.cpp
+++ nTasks/WindowManager.cpp
@@ -1,12 +1,12 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *	WindowManager.cpp												July, 2012
- *	The nModules Project
- *
- *	Monitors all existing top-level windows. Forwards notifications to the
- *	taskbars.
- *      
- *													             Erik Welander
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+*	WindowManager.cpp												July, 2012
+*	The nModules Project
+*
+*	Monitors all existing top-level windows. Forwards notifications to the
+*	taskbars.
+*      
+*													             Erik Welander
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #include "../headers/lsapi.h"
 #include "TaskButton.hpp"
 #include "Taskbar.hpp"
@@ -27,294 +27,298 @@ extern map<LPCSTR, Taskbar*> g_Taskbars;
 extern HWND g_hWndMsgHandler;
 
 namespace WindowManager {
-	// The currently active window
-	HWND activeWindow = NULL;
+    // The currently active window
+    HWND activeWindow = NULL;
 
-	// Contains all current top-level windows.
-	map<HWND, WindowInformation> windowMap;
+    // Contains all current top-level windows.
+    map<HWND, WindowInformation> windowMap;
 
-	// Information about the current monitor layout.
-	MonitorInfo* g_pMonitorInfo = NULL;
+    // Information about the current monitor layout.
+    MonitorInfo* g_pMonitorInfo = NULL;
 
-	// True if the windowmanager is running.
-	bool isStarted = false;
+    // True if the windowmanager is running.
+    bool isStarted = false;
 }
+
 
 /// <summary>
 /// Starts the window manager.
 /// </summary>
 void WindowManager::Start() {
-	// Check that we aren't already running
-	assert(!isStarted);
-	isStarted = true;
+    // Check that we aren't already running
+    assert(!isStarted);
+    isStarted = true;
 
-	// Initalize our monitorinfo class
-	g_pMonitorInfo = new MonitorInfo();
+    // Initalize our monitorinfo class
+    g_pMonitorInfo = new MonitorInfo();
 
-	// Grab all existing top level windows.
-	EnumDesktopWindows(NULL, AddWindow, 1);
+    // Grab all existing top level windows.
+    EnumDesktopWindows(NULL, AddWindow, 1);
 
-	for (TASKBARCITER iter = g_Taskbars.begin(); iter != g_Taskbars.end(); iter++) {
-		iter->second->Relayout();
-	}
+    for (TASKBARCITER iter = g_Taskbars.begin(); iter != g_Taskbars.end(); iter++) {
+        iter->second->Relayout();
+    }
 
-	// Get the currently active window
-	SetActive(GetForegroundWindow());
+    // Get the currently active window
+    SetActive(GetForegroundWindow());
 
-	SetTimer(g_hWndMsgHandler, 1337, 250, NULL);
+    SetTimer(g_hWndMsgHandler, 1337, 250, NULL);
 }
+
 
 /// <summary>
 /// Stops the window manager. Removes all windows from the taskbars and drops
 /// the shellhook.
 /// </summary>
 void WindowManager::Stop() {
-	// Check that we are currently running
-	assert(isStarted);
+    // Check that we are currently running
+    assert(isStarted);
 
-	// Clean up
-	KillTimer(g_hWndMsgHandler, 1337);
-	delete g_pMonitorInfo;
-	activeWindow = NULL;
-	windowMap.clear();
-	isStarted = false;
+    // Clean up
+    KillTimer(g_hWndMsgHandler, 1337);
+    delete g_pMonitorInfo;
+    activeWindow = NULL;
+    windowMap.clear();
+    isStarted = false;
 }
+
 
 /// <summary>
 /// Adds the specified top level window to the list of windows.
 /// </summary>
 BOOL CALLBACK WindowManager::AddWindow(HWND hWnd, LPARAM lParam) {
-	WindowInformation wndInfo;
-	WCHAR szTitle[MAX_LINE_LENGTH];
+    WindowInformation wndInfo;
+    WCHAR szTitle[MAX_LINE_LENGTH];
 
-	// Check that we are currently running
-	assert(isStarted);
+    // Check that we are currently running
+    assert(isStarted);
 
-	// If we should add this window to the taskbars
-	if (IsTaskbarWindow(hWnd)) {
+    // If we should add this window to the taskbars
+    if (IsTaskbarWindow(hWnd)) {
 
-		// Get information about the window
-		GetWindowTextW(hWnd, szTitle, sizeof(szTitle)/sizeof(WCHAR));
-		wndInfo.uMonitor = g_pMonitorInfo->MonitorFromHWND(hWnd);
-		wndInfo.hIcon = GetIcon(hWnd);
+        // Get information about the window
+        GetWindowTextW(hWnd, szTitle, sizeof(szTitle)/sizeof(WCHAR));
+        wndInfo.uMonitor = g_pMonitorInfo->MonitorFromHWND(hWnd);
 
-		// Add it to any taskbar that wants it
-		for (TASKBARCITER iter = g_Taskbars.begin(); iter != g_Taskbars.end(); iter++) {
-			TaskButton* taskButton = iter->second->AddTask(hWnd, wndInfo.uMonitor, lParam == 1);
+        // Add it to any taskbar that wants it
+        for (TASKBARCITER iter = g_Taskbars.begin(); iter != g_Taskbars.end(); iter++) {
+            TaskButton* taskButton = iter->second->AddTask(hWnd, wndInfo.uMonitor, lParam == 1);
 
-			// If the taskbar createad a button for this window
-			if (taskButton != NULL) {
-				// Add it to our list of buttons
-				wndInfo.buttons.push_back(taskButton);
+            // If the taskbar created a button for this window
+            if (taskButton != NULL) {
+                // Add it to our list of buttons
+                wndInfo.buttons.push_back(taskButton);
 
-				// Set the icon and text of the window.
-				taskButton->SetIcon(wndInfo.hIcon);
-				taskButton->SetText(szTitle);
-			}
-		}
+                // Set the icon and text of the window.
+                taskButton->SetText(szTitle);
+            }
+        }
 
-		windowMap.insert(pair<HWND, WindowInformation>(hWnd, wndInfo));
-	}
+        windowMap.insert(pair<HWND, WindowInformation>(hWnd, wndInfo));
+        UpdateIcon(hWnd);
+    }
 
-	return TRUE;
+    return TRUE;
 }
+
 
 /// <summary>
 /// Updates the currently active window.
 /// </summary>
 void WindowManager::SetActive(HWND hWnd) {
-	// Check that we are currently running
-	assert(isStarted);
+    // Check that we are currently running
+    assert(isStarted);
 
-	// Remove the active flag from the previously active buttons
-	WNDMAPCITER iter = windowMap.find(activeWindow);
-	if (iter != windowMap.end()) {
-		for (vector<TaskButton*>::const_iterator iter2 = iter->second.buttons.begin(); iter2 != iter->second.buttons.end(); iter2++) {
-			(*iter2)->Deactivate();
-		}
-	}
+    // Remove the active flag from the previously active buttons
+    WNDMAPCITER iter = windowMap.find(activeWindow);
+    if (iter != windowMap.end()) {
+        for (vector<TaskButton*>::const_iterator iter2 = iter->second.buttons.begin(); iter2 != iter->second.buttons.end(); iter2++) {
+            (*iter2)->Deactivate();
+        }
+    }
 
-	// Swap the active window
-	activeWindow = hWnd;
+    // Swap the active window
+    activeWindow = hWnd;
 
-	// And set the flag for the now active buttons
-	iter = windowMap.find(activeWindow);
-	if (iter != windowMap.end()) {
-		for (vector<TaskButton*>::const_iterator iter2 = iter->second.buttons.begin(); iter2 != iter->second.buttons.end(); iter2++) {
-			(*iter2)->Activate();
-		}
-	}
+    // And set the flag for the now active buttons
+    iter = windowMap.find(activeWindow);
+    if (iter != windowMap.end()) {
+        for (vector<TaskButton*>::const_iterator iter2 = iter->second.buttons.begin(); iter2 != iter->second.buttons.end(); iter2++) {
+            (*iter2)->Activate();
+        }
+    }
 }
+
 
 /// <summary>
 /// Removes the specified window from all taskbars.
 /// </summary>
 void WindowManager::RemoveWindow(HWND hWnd) {
-	// Check that we are currently running
-	assert(isStarted);
+    // Check that we are currently running
+    assert(isStarted);
 
-	WNDMAPITER iter = windowMap.find(hWnd);
-	if (iter != windowMap.end()) {
-		// Remove all buttons
-		for (TASKBARCITER iter2 = g_Taskbars.begin(); iter2 != g_Taskbars.end(); iter2++) {
-			iter2->second->RemoveTask(hWnd);
-		}
+    WNDMAPITER iter = windowMap.find(hWnd);
+    if (iter != windowMap.end()) {
+        // Remove all buttons
+        for (TASKBARCITER iter2 = g_Taskbars.begin(); iter2 != g_Taskbars.end(); iter2++) {
+            iter2->second->RemoveTask(hWnd);
+        }
 
-		windowMap.erase(iter);
-	}
+        windowMap.erase(iter);
+    }
 
-	if (activeWindow == hWnd)
-		activeWindow = NULL;
+    if (activeWindow == hWnd)
+        activeWindow = NULL;
 }
+
 
 /// <summary>
 /// Removes the specified window from all taskbars.
 /// </summary>
 void WindowManager::UpdateWindow(HWND hWnd, LPARAM lParam) {
-	// Check that we are currently running
-	assert(isStarted);
+    // Check that we are currently running
+    assert(isStarted);
 
-	WNDMAPITER iter = windowMap.find(hWnd);
-	if (iter != windowMap.end()) {
-		// Update the text
-		WCHAR szTitle[MAX_LINE_LENGTH];
-		GetWindowTextW(hWnd, szTitle, sizeof(szTitle)/sizeof(WCHAR));
-		for (vector<TaskButton*>::const_iterator iter2 = iter->second.buttons.begin(); iter2 != iter->second.buttons.end(); iter2++) {
-			(*iter2)->SetText(szTitle);
-		}
+    WNDMAPITER iter = windowMap.find(hWnd);
+    if (iter != windowMap.end()) {
+        // Update the text
+        WCHAR szTitle[MAX_LINE_LENGTH];
+        GetWindowTextW(hWnd, szTitle, sizeof(szTitle)/sizeof(WCHAR));
+        for (vector<TaskButton*>::const_iterator iter2 = iter->second.buttons.begin(); iter2 != iter->second.buttons.end(); iter2++) {
+            (*iter2)->SetText(szTitle);
+        }
 
-		// Update the icon
-		HICON hIcon = GetIcon(hWnd);
-		if (hIcon != iter->second.hIcon) {
-			for (vector<TaskButton*>::const_iterator iter2 = iter->second.buttons.begin(); iter2 != iter->second.buttons.end(); iter2++) {
-				(*iter2)->SetIcon(hIcon);
-			}
-			iter->second.hIcon = hIcon;
-		}
+        // Update the icon
+        UpdateIcon(hWnd);
 
-		// Check if we should be flashing
-		if (lParam == TRUE) {
-			for (vector<TaskButton*>::const_iterator iter2 = iter->second.buttons.begin(); iter2 != iter->second.buttons.end(); iter2++) {
-				(*iter2)->Flash();
-			}
-		}
-	}
+        // Check if we should be flashing
+        if (lParam == TRUE) {
+            for (vector<TaskButton*>::const_iterator iter2 = iter->second.buttons.begin(); iter2 != iter->second.buttons.end(); iter2++) {
+                (*iter2)->Flash();
+            }
+        }
+    }
 }
+
 
 /// <summary>
 /// Retrives the rectangle of the first taskbutton for the specified HWND.
 /// </summary>
 LRESULT WindowManager::GetMinRect(HWND hWnd, LPPOINTS lpPoints) {
-	// Check that we are currently running
-	assert(isStarted);
+    // Check that we are currently running
+    assert(isStarted);
 
-	WNDMAPCITER iter = windowMap.find(hWnd);
-	if (iter != windowMap.end() && !iter->second.buttons.empty()) {
-		iter->second.buttons[0]->GetMinRect(lpPoints);
-		return 1;
-	}
-	return 0;
+    WNDMAPCITER iter = windowMap.find(hWnd);
+    if (iter != windowMap.end() && !iter->second.buttons.empty()) {
+        iter->second.buttons[0]->GetMinRect(lpPoints);
+        return 1;
+    }
+    return 0;
 }
+
 
 /// <summary>
 /// Updates the monitor information for each window.
 /// </summary>
 void WindowManager::UpdateMonitor(WNDMAPITER iter) {
-	if (iter != windowMap.end()) {
-		int monitor = g_pMonitorInfo->MonitorFromHWND(iter->first);
-		if ((UINT)monitor != iter->second.uMonitor) {
-			HWND hWnd = iter->first;
-			RemoveWindow(hWnd);
-			AddWindow(hWnd, NULL);
-		}
-	}
+    if (iter != windowMap.end()) {
+        int monitor = g_pMonitorInfo->MonitorFromHWND(iter->first);
+        if ((UINT)monitor != iter->second.uMonitor) {
+            HWND hWnd = iter->first;
+            RemoveWindow(hWnd);
+            AddWindow(hWnd, NULL);
+        }
+    }
 }
+
 
 /// <summary>
 /// Updates the monitor information for each window.
 /// </summary>
 void WindowManager::UpdateWindowMonitors() {
-	vector<HWND> mods;
-	for (WNDMAPITER iter = windowMap.begin(); iter != windowMap.end(); iter++) {
-		int monitor = g_pMonitorInfo->MonitorFromHWND(iter->first);
-		if ((UINT)monitor != iter->second.uMonitor) {
-			mods.push_back(iter->first);
-		}
-	}
-	for (vector<HWND>::iterator iter = mods.begin(); iter != mods.end(); iter++) {
-		RemoveWindow(*iter);
-		AddWindow(*iter, NULL);
-	}
+    vector<HWND> mods;
+    for (WNDMAPITER iter = windowMap.begin(); iter != windowMap.end(); iter++) {
+        int monitor = g_pMonitorInfo->MonitorFromHWND(iter->first);
+        if ((UINT)monitor != iter->second.uMonitor) {
+            mods.push_back(iter->first);
+        }
+    }
+    for (vector<HWND>::iterator iter = mods.begin(); iter != mods.end(); iter++) {
+        RemoveWindow(*iter);
+        AddWindow(*iter, NULL);
+    }
 }
+
 
 /// <summary>
 /// Processes shell hook messages.
 /// </summary>
 LRESULT WindowManager::ShellMessage(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	// Check that we are currently running
-	assert(isStarted);
+    // Check that we are currently running
+    assert(isStarted);
 
-	switch (uMsg) {
+    switch (uMsg) {
 
-	// A window is being minimized or restored, the system needs the coordinates of
-	// the taskbutton for the window animation.
-	case LM_GETMINRECT:
-		return GetMinRect((HWND)wParam, (LPPOINTS)lParam);
+        // A window is being minimized or restored, the system needs the coordinates of
+        // the taskbutton for the window animation.
+    case LM_GETMINRECT:
+        return GetMinRect((HWND)wParam, (LPPOINTS)lParam);
 
-	// The active input language has changed
-	case LM_LANGUAGE:
-		return 0;
+        // The active input language has changed
+    case LM_LANGUAGE:
+        return 0;
 
-	// Text/Icon/Blinking change
-	case LM_REDRAW:
-		UpdateWindow((HWND)wParam, lParam);
-		return 0;
-	
-	// The active window has changed
-	case LM_WINDOWACTIVATED:
-		SetActive((HWND)wParam);
-		return 0;
+        // Text/Icon/Blinking change
+    case LM_REDRAW:
+        UpdateWindow((HWND)wParam, lParam);
+        return 0;
 
-	// A new top level window has been created
-	case LM_WINDOWCREATED:
-		AddWindow((HWND)wParam, NULL);
-		return 0;
+        // The active window has changed
+    case LM_WINDOWACTIVATED:
+        SetActive((HWND)wParam);
+        return 0;
 
-	// A top level window has been destroyed
-	case LM_WINDOWDESTROYED:
-		RemoveWindow((HWND)wParam);
-		return 0;
+        // A new top level window has been created
+    case LM_WINDOWCREATED:
+        AddWindow((HWND)wParam, NULL);
+        return 0;
 
-	// A top level window has been replaced
-	case LM_WINDOWREPLACED:
-		AddWindow((HWND)wParam, NULL);
-		return 0;
+        // A top level window has been destroyed
+    case LM_WINDOWDESTROYED:
+        RemoveWindow((HWND)wParam);
+        return 0;
 
-	// A window is about to be replaced
-	case LM_WINDOWREPLACING:
-		RemoveWindow((HWND)wParam);
-		return 0;
+        // A top level window has been replaced
+    case LM_WINDOWREPLACED:
+        AddWindow((HWND)lParam, NULL);
+        return 0;
 
-	// Windows 8+ A window has moved to a different monitor
-	case LM_MONITORCHANGED:
-		return 0;
+        // A window is about to be replaced
+    case LM_WINDOWREPLACING:
+        RemoveWindow((HWND)wParam);
+        return 0;
 
-	// The display layout has changed.
-	case WM_DISPLAYCHANGE:
-		g_pMonitorInfo->Update();
-		UpdateWindowMonitors();
-		return 0;
+        // Windows 8+ A window has moved to a different monitor
+    case LM_MONITORCHANGED:
+        return 0;
 
-	//
-	case WM_TIMER:
-		switch(wParam) {
-		case 1337:
-			UpdateWindowMonitors();
-			return 0;
-		}
-	}
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+        // The display layout has changed.
+    case WM_DISPLAYCHANGE:
+        g_pMonitorInfo->Update();
+        UpdateWindowMonitors();
+        return 0;
+
+        //
+    case WM_TIMER:
+        switch(wParam) {
+        case 1337:
+            UpdateWindowMonitors();
+            return 0;
+        }
+    }
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
+
 
 /// <summary>
 /// Processes callwndproc hook messages.
@@ -322,46 +326,91 @@ LRESULT WindowManager::ShellMessage(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM 
 //LRESULT WindowManager::ShellMessageHook() {
 //}
 
+
 /// <summary>
 /// Determines if a window should be shown on the taskbar.
 /// </summary>
 bool WindowManager::IsTaskbarWindow(HWND hWnd) {
-	// Make sure it's actually a window.
-	if (!IsWindow(hWnd))
-		return false;
+    // Make sure it's actually a window.
+    if (!IsWindow(hWnd))
+        return false;
 
-	// And that it's visible
-	if (!IsWindowVisible(hWnd))
-		return false;
+    // And that it's visible
+    if (!IsWindowVisible(hWnd))
+        return false;
 
-	LONG_PTR exStyle = GetWindowLongPtr(hWnd, GWL_EXSTYLE);
+    LONG_PTR exStyle = GetWindowLongPtr(hWnd, GWL_EXSTYLE);
 
-	// Windows with the WS_EX_APPWINDOW style should always be shown
-	if ((exStyle & WS_EX_APPWINDOW) == WS_EX_APPWINDOW)
-		return true;
-	else if (GetParent(hWnd) != NULL) // Windows with parents should not be shown
-		return false;
-	else if (GetWindow(hWnd, GW_OWNER) != NULL) // Windows with owner should not be shown
-		return false;
-	else if ((exStyle & WS_EX_TOOLWINDOW) == WS_EX_TOOLWINDOW) // Toolwindows should not be shown on the taskbar
-		return false;
+    // Windows with the WS_EX_APPWINDOW style should always be shown
+    if ((exStyle & WS_EX_APPWINDOW) == WS_EX_APPWINDOW)
+        return true;
+    else if (GetParent(hWnd) != NULL) // Windows with parents should not be shown
+        return false;
+    else if (GetWindow(hWnd, GW_OWNER) != NULL) // Windows with owner should not be shown
+        return false;
+    else if ((exStyle & WS_EX_TOOLWINDOW) == WS_EX_TOOLWINDOW) // Toolwindows should not be shown on the taskbar
+        return false;
 
-	return true;
+    return true;
 }
 
+
 /// <summary>
-/// Gets the icon of a particular HWND
+/// Updates the icon of a particular HWND
 /// </summary>
-HICON WindowManager::GetIcon(HWND hWnd) {
-	HICON hIcon = NULL;
-	hIcon = (HICON)SendMessage(hWnd, WM_GETICON, ICON_BIG, NULL);
-	if (hIcon != NULL) return hIcon;
-	hIcon = (HICON)SendMessage(hWnd, WM_GETICON, ICON_SMALL, NULL);
-	if (hIcon != NULL) return hIcon;
-	hIcon = (HICON)SendMessage(hWnd, WM_GETICON, ICON_SMALL2, NULL);
-	if (hIcon != NULL) return hIcon;
-	hIcon = (HICON)GetClassLongPtr(hWnd, GCLP_HICON);
-	if (hIcon != NULL) return hIcon;
-	hIcon = (HICON)GetClassLongPtr(hWnd, GCLP_HICONSM);
-	return hIcon;
+void WindowManager::SetIcon(HWND hWnd, HICON hIcon) {
+    WNDMAPITER window = windowMap.find(hWnd);
+    if (window != windowMap.end()) {
+        if (hIcon != window->second.hIcon) {
+            for (vector<TaskButton*>::const_iterator iter = window->second.buttons.begin(); iter != window->second.buttons.end(); iter++) {
+                (*iter)->SetIcon(hIcon);
+            }
+            window->second.hIcon = hIcon;
+        }
+    }
+}
+
+
+/// <summary>
+/// Updates the icon of a particular HWND
+/// </summary>
+void CALLBACK WindowManager::UpdateIconCallback(HWND hWnd, UINT uMsg, ULONG_PTR dwData, LRESULT lResult) {
+    // We really only expect WM_GETICON messages.
+    if (uMsg == WM_GETICON) {
+        // If we got an icon back, use it.
+        if (lResult != NULL) {
+            SetIcon(hWnd, (HICON)lResult);
+        }
+        else switch (dwData) {
+        case ICON_BIG:
+            {
+                SendMessageCallback(hWnd, WM_GETICON, ICON_SMALL, NULL, UpdateIconCallback, ICON_SMALL);
+            }
+            break;
+
+        case ICON_SMALL:
+            {
+                SendMessageCallback(hWnd, WM_GETICON, ICON_SMALL2, NULL, UpdateIconCallback, ICON_SMALL2);
+            }
+            break;
+
+        case ICON_SMALL2:
+            {
+                HICON hIcon;
+                hIcon = (HICON)GetClassLongPtr(hWnd, GCLP_HICON);
+                if (!hIcon)
+                    hIcon = (HICON)GetClassLongPtr(hWnd, GCLP_HICONSM);
+                SetIcon(hWnd, hIcon);
+            }
+            break;
+        }
+    }
+}
+
+
+/// <summary>
+/// Updates the icon of a particular HWND
+/// </summary>
+void WindowManager::UpdateIcon(HWND hWnd) {
+    SendMessageCallback(hWnd, WM_GETICON, ICON_BIG, NULL, UpdateIconCallback, ICON_BIG);
 }
--- nTasks/WindowManager.h
+++ nTasks/WindowManager.h
@@ -1,41 +1,43 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *	WindowManager.h													July, 2012
- *	The nModules Project
- *
- *	Monitors all existing top-level windows.
- *      
- *													             Erik Welander
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+*  WindowManager.h                                                  July, 2012
+*  The nModules Project
+*
+*  Monitors all existing top-level windows.
+*      
+*  Erik Welander
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #ifndef WINDOWMANAGER_H
 #define WINDOWMANAGER_H
 
 namespace WindowManager {
-	/// <summary>
-	/// Holds all information about a particular top level window.
-	/// </summary>
-	typedef struct WindowInformation {
-		UINT uMonitor;
-		vector<TaskButton*> buttons;
-		HICON hIcon;
-	} WindowInformation;
+    /// <summary>
+    /// Holds all information about a particular top level window.
+    /// </summary>
+    typedef struct WindowInformation {
+        UINT uMonitor;
+        vector<TaskButton*> buttons;
+        HICON hIcon;
+    } WindowInformation;
 
-	// Some helpful typedefs
-	typedef map<LPCSTR, Taskbar*>::const_iterator TASKBARCITER;
-	typedef map<HWND, WindowInformation>::const_iterator WNDMAPCITER;
-	typedef map<HWND, WindowInformation>::iterator WNDMAPITER;
+    // Some helpful typedefs
+    typedef map<LPCSTR, Taskbar*>::const_iterator TASKBARCITER;
+    typedef map<HWND, WindowInformation>::const_iterator WNDMAPCITER;
+    typedef map<HWND, WindowInformation>::iterator WNDMAPITER;
 
-	void Start();
-	void Stop();
-	bool IsTaskbarWindow(HWND hWnd);
-	void SetActive(HWND hWnd);
-	LRESULT ShellMessage(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-	BOOL CALLBACK AddWindow(HWND hWnd, LPARAM lParam);
-	void RemoveWindow(HWND hWnd);
-	LRESULT GetMinRect(HWND, LPPOINTS);
-	HICON GetIcon(HWND hWnd);
-	void UpdateWindow(HWND hWnd, LPARAM data);
-	void UpdateWindowMonitors();
-	void UpdateMonitor(WNDMAPITER iter);
+    void Start();
+    void Stop();
+    bool IsTaskbarWindow(HWND hWnd);
+    void SetActive(HWND hWnd);
+    LRESULT ShellMessage(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    BOOL CALLBACK AddWindow(HWND hWnd, LPARAM lParam);
+    void RemoveWindow(HWND hWnd);
+    LRESULT GetMinRect(HWND, LPPOINTS);
+    void UpdateIcon(HWND hWnd);
+    void CALLBACK UpdateIconCallback(HWND hWnd, UINT uMsg, ULONG_PTR dwData, LRESULT lResult);
+    void SetIcon(HWND, HICON);
+    void UpdateWindow(HWND hWnd, LPARAM data);
+    void UpdateWindowMonitors();
+    void UpdateMonitor(WNDMAPITER iter);
 }
 
 #endif /* WINDOWMANAGER_H */
