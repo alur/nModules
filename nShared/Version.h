705d364b95a3da564b40bf05bca8c0d9814254c0
Erik Welander <alurcard2@gmail.com>
2013-08-12 15:27:14 -0700

    Bumped the version number

--- nShared/Version.h
+++ nShared/Version.h
@@ -8,10 +8,10 @@
 #pragma once
 
 // The version of the shared components
-#define SHARED_VERSION 0, 5, 0, 0
+#define SHARED_VERSION 0, 5, 5, 0
 
 // The minimum core version required
-#define SHARED_CORE_VERSION 0, 5, 0, 0
+#define SHARED_CORE_VERSION 0, 5, 5, 0
 
 // String version of the SHARED_VERSION
-#define SHARED_VERSION_STR "0.5"
+#define SHARED_VERSION_STR "0.5.5"
